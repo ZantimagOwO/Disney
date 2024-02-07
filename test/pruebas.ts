03d681024f6431ce04e75a0fcc14431b8cc512fd
Santiago Gimeno Julián <85684226+ZantimagOwO@users.noreply.github.com>
2024-02-07 12:00:46 +0100

    replaced with alberto typeorm

--- test/pruebas.ts
+++
@@ -1,7 +0,0 @@
-console.log("hola");
-
-console.log('Ejecutando pruebas de TypeScript');
-
-function sum(a: number, b: number): number {
-    return a + b;
-}
--- test/sum.spec.ts
+++
@@ -1,7 +0,0 @@
-function sum(a: number, b: number): number {
-  return a + b;
-}
-
-test("sumar 1 y 2", () => {
-  expect(sum(1, 2)).toBe(3);
-});
